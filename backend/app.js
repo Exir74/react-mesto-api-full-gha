fedf0d7fdec9e95b573338e80e6ef686c9af0ad7
Exir74 <exir74@gmail.com>
2023-07-16 17:30:56 +0500

    host

--- backend/app.js
+++ backend/app.js
@@ -8,22 +8,11 @@ const errorHandler = require('./errors/errorHandler');
 const routes = require('./routes/index');
 const { URL, PORT } = require('./utils/constants');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const allowCors = [
-  '127.0.0.1:3000',
-  'localhost:3000',
-  'http://api.exir74.nomoredomains.xyz',
-  'https://api.exir74.nomoredomains.xyz'
-]
+const cors = require('cors')
 
 
 const app = express();
-app.use(function (req, res, next){
-  const {origin} = req.headers;
-  if (allowCors.includes(origin)){
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-  next()
-})
+app.use(cors())
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
