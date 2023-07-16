d758fc5ac60478e6a66deb9fdcfece94704b6f97
Exir74 <exir74@gmail.com>
2023-07-16 19:36:31 +0500

    host

--- backend/app.js
+++ backend/app.js
@@ -13,8 +13,8 @@ const cors = require('cors')
 
 const app = express();
 
-app.use(cors())
 app.use(helmet());
+app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
