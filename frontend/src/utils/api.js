20ce3fb90607bc441a4d8b24a3ced577d64fefce
Exir74 <exir74@gmail.com>
2023-07-17 20:51:06 +0500

    host1

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -29,7 +29,6 @@ class Api {
       method: 'GET',
       credentials: 'include',
       headers: {
-        'Accept': 'application/json',
         'Content-Type': 'application/json'
       },
     })
