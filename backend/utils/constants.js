265c1ba2df65640ca4d29fca7314df1940145685
Exir74 <exir74@gmail.com>
2023-07-17 13:27:09 +0500

    host1

--- backend/utils/constants.js
+++ backend/utils/constants.js
@@ -6,7 +6,7 @@ const FORBIDDEN = 403;
 const CONFLICT = 409;
 const SERVER_ERROR = 500;
 const URL = 'mongodb://127.0.0.1:27017/mestodb';
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 
 module.exports = {
   JWT_SECRET,
