4209c65fd4b6a0d5b3d28587db939d92015e3f31
Exir74 <exir74@gmail.com>
2023-07-17 21:48:49 +0500

    host1

--- backend/app.js
+++ backend/app.js
@@ -28,6 +28,7 @@ app.use(function(req, res, next) {
   const { origin } = req.headers;
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
+
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', 'true');
--- backend/controllers/userControls.js
+++ backend/controllers/userControls.js
@@ -109,6 +109,7 @@ module.exports.login = (req, res, next) => {
         .cookie('jwt', token, {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
+          sameSite: true
         })
         .send(user)
         .end();
