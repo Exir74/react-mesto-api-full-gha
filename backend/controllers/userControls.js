62c3759e34a3ca0f57d09b79dacbaecb368d8caa
Exir74 <exir74@gmail.com>
2023-07-19 21:30:33 +0500

    for tests1

--- backend/controllers/userControls.js
+++ backend/controllers/userControls.js
@@ -110,8 +110,8 @@ module.exports.login = (req, res, next) => {
         .cookie('jwt', token, {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
-          // secure: true,
-          // sameSite: 'none',
+          secure: true,
+          sameSite: 'none',
         })
         .send({ token })
         .end();
