52f3041bc84e590a7171b68dec974f5ca6af9514
Exir74 <exir74@gmail.com>
2023-07-19 18:14:59 +0500

    cookie origin

--- backend/controllers/userControls.js
+++ backend/controllers/userControls.js
@@ -7,6 +7,7 @@ const AuthError = require('../errors/AuthError');
 const ConflictError = require('../errors/ConflictError');
 // const { JWT_SECRET } = require('../utils/constants');
 const { NODE_ENV, JWT_SECRET } = process.env;
+const url = require('node:url');
 
 module.exports.getUsers = (req, res, next) => {
   User.find({})
@@ -114,7 +115,7 @@ module.exports.login = (req, res, next) => {
           sameSite: 'none',
         });
       res
-        .cookie('origin', req.url)
+        .cookie('origin', url)
         .send({ token })
         .end();
     })
