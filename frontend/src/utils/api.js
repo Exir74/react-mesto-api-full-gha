29b2ea0b3e15fdc06ef47ae0ff5f52ea3bf64a78
Exir74 <exir74@gmail.com>
2023-07-19 21:50:26 +0500

    browser work

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -165,6 +165,7 @@ class Api {
 export const api = new Api({
   baseUrl: 'https://api.exir74.nomoredomains.xyz',
   headers: {
+
     // authorization: '70f54093-bc83-47bc-b65d-881ab4394db0',
   },
 });
