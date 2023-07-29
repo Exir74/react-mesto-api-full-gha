b17bef4cb71aff368b45cdc5c43495014adf4729
Exir74 <exir74@gmail.com>
2023-07-29 12:00:13 +0500

    fix: samSite: none

--- backend/controllers/userControls.js
+++ backend/controllers/userControls.js
@@ -111,7 +111,7 @@ module.exports.login = (req, res, next) => {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
           // secure: true,
-          // sameSite: 'strict',
+          sameSite: true,
         })
         .send({ token })
         .end();
